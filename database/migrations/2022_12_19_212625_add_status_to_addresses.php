b3476c4f91392318f21f556560af83a7097dad3f
a.anbari <amiranbari33@gmail.com>
2022-12-20 01:01:13 +0330

    add status into addresses

---
+++ database/migrations/2022_12_19_212625_add_status_to_addresses.php
@@ -0,0 +1,33 @@
+<?php
+
+use App\Enums\AddressStatus;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('addresses', function (Blueprint $table) {
+            $table->enum('status', AddressStatus::getAllValues())->default(AddressStatus::DISABLE->name);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('addresses', function (Blueprint $table) {
+            $table->dropColumn('status');
+        });
+    }
+};
